04a49e6327703ddee6d0eae6e1b424b457f0ff70
Akash Sharma <lucky1612000@gmail.com>
2020-12-10 17:17:31 +0530

    Added a comment

--- dsa_ninjas/merge_sort_linked_list.cpp
+++ dsa_ninjas/merge_sort_linked_list.cpp
@@ -36,6 +36,8 @@ Node* merge(Node *head1, Node *head2) {
     Node *final_head, *final_tail, *save;
     final_head = final_tail = (head1 -> data <= head2 -> data) ? head1 : head2;
     while(head1 != nullptr && head2 != nullptr) {
+        // the condition head1 != nullptr handles the conditon
+        // when any list exhausts while we are traversing it
         if(head1 != nullptr && (head1 -> data <= head2 -> data)) {
             save = head1;
             while(head1 -> data <= head2 -> data) {
@@ -86,6 +88,6 @@ Node* merge_sort(Node *head) {
 }
 
 int main() {
-    Node *head = new Node(4, new Node(1, new Node(3, new Node(2, new Node(0)))));
+    Node *head = new Node(4, new Node(1, new Node(-3, new Node(2, new Node(0)))));
     print_linked_list(merge_sort(head));
 }
