98131966d02a7ab01da1ccc3bf5aff25a3d5cab4
Akash Sharma <lucky1612000@gmail.com>
2020-12-12 14:52:53 +0530

    Better code for linked list reversal.

--- dsa_ninjas/reverse_linked_list.cpp
+++ dsa_ninjas/reverse_linked_list.cpp
@@ -35,7 +35,21 @@ Node* reverse(Node *current, Node *previous = nullptr) {
     return save;
 }
 
+Node* reverse_improved(Node *current) {
+    // the first check is for detecting empty linked list
+    // the second check is essential
+    if(current == nullptr || current -> next == nullptr) {
+        return current;
+    }
+    Node *save = reverse_improved(current -> next);
+    current -> next -> next = current;
+    // since we cannot know when we are dealing with
+    // first node so we set nullptr for all
+    current -> next = nullptr; 
+    return save;
+}
+
 int main() {
     Node *head = new Node(4, new Node(1, new Node(-3, new Node(2, new Node(0)))));
-    print_linked_list(reverse(head));
+    print_linked_list(reverse_improved(head));
 }
