13dfe2d1a9f07a2a7d82184e075787ed4d48f897
Akash Sharma <lucky1612000@gmail.com>
2021-09-30 19:02:09 +0530

    Java does not support default values for arguments �

---
+++ dsa_ninjas_java/FindFactorial.java
@@ -0,0 +1,14 @@
+public class FindFactorial {
+    public static int fact(int x) {
+        if(x == 1) {
+            return 1;
+        }
+        int smallAns = fact(x-1);
+        return x * smallAns;
+    }
+    
+    public static void main(String args[]) {
+        int n = 3;
+        System.out.println(fact(n));
+    }
+}
