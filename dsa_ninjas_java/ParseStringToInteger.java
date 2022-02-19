6f1e13cd051fa181b07022bc217c702c575448af
Akash Sharma <lucky1612000@gmail.com>
2022-02-19 14:30:23 +0530

    more ways of doing things

--- dsa_ninjas_java/ParseStringToInteger.java
+++ dsa_ninjas_java/ParseStringToInteger.java
@@ -1,6 +1,20 @@
 import java.util.Arrays;
 
-public class ParseStringToInteger {
+class ParseStringToIntegerV1 {
+    public static int parse(String source, int currentIndex) {
+        if (currentIndex == source.length()) {
+            return 0;
+        }
+        return (source.charAt(currentIndex) - '0') * (int) Math.pow(10, source.length() - currentIndex - 1)
+                + parse(source, currentIndex + 1);
+    }
+
+    public static int parse(String source) {
+        return parse(source, 0);
+    }
+}
+
+class ParseStringToIntegerV2 {
     public static int parse(String source, int currentIndex) {
         // requires 1 less argument to be passed while calling
         if (currentIndex == source.length()) {
@@ -10,6 +24,12 @@ public class ParseStringToInteger {
                 + (source.charAt(currentIndex) - '0') * (int) Math.pow(10, source.length() - currentIndex - 1);
     }
 
+    public static int parse(String source) {
+        return parse(source, 0);
+    }
+}
+
+class ParseStringToIntegerV3 {
     public static int parse(String source, int result, int currentIndex) {
         if (currentIndex == source.length()) {
             return result;
@@ -17,17 +37,16 @@ public class ParseStringToInteger {
         return parse(source, (result * 10) + source.charAt(currentIndex) - '0', currentIndex + 1);
     }
 
-    public static int parsev1(String source) {
+    public static int parse(String source) {
         return parse(source, 0, 0);
     }
+}
 
-    public static int parsev2(String source) {
-        return parse(source, 0);
-    }
-
+class Test {
     public static void main(String[] args) {
         String[] testValues = { "78965", "12321", "1221", "1721", "78488487", "7", "" };
-        Arrays.stream(testValues).forEach(s -> System.out.println(parsev1(s)));
-        Arrays.stream(testValues).forEach(s -> System.out.println(parsev2(s)));
+        Arrays.stream(testValues).forEach(s -> System.out.println(ParseStringToIntegerV1.parse(s)));
+        Arrays.stream(testValues).forEach(s -> System.out.println(ParseStringToIntegerV2.parse(s)));
+        Arrays.stream(testValues).forEach(s -> System.out.println(ParseStringToIntegerV3.parse(s)));
     }
 }
