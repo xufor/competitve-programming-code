58f05262db2d5dad3da785d95346ddcd6fc0e82d
Akash Sharma <lucky1612000@gmail.com>
2022-03-05 23:38:51 +0530

    permutations

--- dsa_ninjas_java/PrintPermutations.java
+++ dsa_ninjas_java/PrintPermutations.java
@@ -1,4 +1,4 @@
-public class PrintPermutations {
+class PrintPermutationsV1 {
     public static void permutations(String source, String result) {
         if (source.isEmpty()) {
             System.out.println(result);
@@ -12,8 +12,36 @@ public class PrintPermutations {
     public static void permutations(String targetString) {
         permutations(targetString, "");
     }
+}
 
+class PrintPermutationsV2 {
+    public static void swap(StringBuilder targetString, int index1, int index2) {
+        char temp = targetString.charAt(index1);
+        targetString.setCharAt(index1, targetString.charAt(index2));
+        targetString.setCharAt(index2, temp);
+    }
+
+    public static void permutations(StringBuilder source, int currentIndex) {
+        if (source.length() == currentIndex) {
+            System.out.println(source);
+            return;
+        }
+        for (int i = currentIndex; i < source.length(); i++) {
+            swap(source, currentIndex, i);
+            permutations(source, currentIndex + 1);
+            swap(source, currentIndex, i);
+        }
+    }
+
+    public static void permutations(String targetString) {
+        permutations(new StringBuilder(targetString), 0);
+    }
+}
+
+class Main {
     public static void main(String[] args) {
-        permutations("abc");
+        PrintPermutationsV1.permutations("abc");
+        System.out.println();
+        PrintPermutationsV2.permutations("abc");
     }
 }
