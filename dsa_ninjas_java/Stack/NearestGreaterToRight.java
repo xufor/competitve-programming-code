9b2529caeba1aceb855053351b4ad997fbecba17
Akash Sharma <lucky1612000@gmail.com>
2022-04-01 22:26:09 +0530

    enhancements

--- dsa_ninjas_java/Stack/NearestGreaterToRight.java
+++ dsa_ninjas_java/Stack/NearestGreaterToRight.java
@@ -6,17 +6,16 @@ public class NearestGreaterToRight {
     public static ArrayList<Integer> nearestGreaterToRight(Integer[] input) {
         Stack<Integer> stack = new Stack<>();
         ArrayList<Integer> result = new ArrayList<>();
-        for (Integer currrentIndex = input.length - 1; currrentIndex > 0; currrentIndex--) {
-            while (!stack.empty() && stack.peek() <= input[currrentIndex]) {
+        for (Integer currentIndex = input.length - 1; currentIndex > 0; currentIndex--) {
+            while (!stack.empty() && stack.peek() <= input[currentIndex]) {
                 stack.pop();
             }
             if (stack.size() == 0)
                 result.add(-1);
             else
                 result.add(stack.peek());
-            stack.push(input[currrentIndex]);
+            stack.push(input[currentIndex]);
         }
-        System.out.println(stack);
         return result;
     }
 
@@ -24,7 +23,7 @@ public class NearestGreaterToRight {
         Integer[][] testArrays = new Integer[][] { { 5, 4, 11, 9, 2, 20, 4, 7, 8, 5 },
                 { 4, 3, 1, 7, 8, 9, 10, 25, 4, 7, 8, 9 } };
         for (Integer[] testArray : testArrays) {
-            ArrayList<Integer> result =  nearestGreaterToRight(testArray);
+            ArrayList<Integer> result = nearestGreaterToRight(testArray);
             Collections.reverse(result);
             System.out.println(result);
         }
