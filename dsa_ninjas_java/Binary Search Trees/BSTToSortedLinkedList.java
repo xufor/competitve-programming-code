3bc35f2b430476d6ae4fa7b68381423ee3263d5f
Akash Sharma <lucky1612000@gmail.com>
2022-04-12 09:48:20 +0530

    bst to ll

---
+++ dsa_ninjas_java/Binary Search Trees/BSTToSortedLinkedList.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+
+public class BSTToSortedLinkedList {
+    public static LinkedList<Integer> convert(BSTNode<Integer> root) {
+        if(root == null) {
+            return new LinkedList<>();
+        }
+
+        var leftResult = convert(root.left);
+        var rightResult = convert(root.right);
+        var nextResult = new LinkedList<Integer>();
+
+        nextResult.addAll(leftResult);
+        nextResult.add(root.data);
+        nextResult.addAll(rightResult);
+        return nextResult;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        for (int data : new Integer[] { 9, 4, 5, 2, 3, 7, 8, 1, 6, 10 })
+            binarySearchTree.insert(data);
+        System.out.println(convert(binarySearchTree.root));
+    }
+}
