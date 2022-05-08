5c78392d18a5926070904bb331982063f4350ab6
Akash Sharma <lucky1612000@gmail.com>
2022-05-08 18:44:42 +0530

    topo sort

---
+++ dsa_ninjas_java/Graphs/topological_sort_using_dfs.py
@@ -0,0 +1,40 @@
+def bfs(adjacency_list, current_vertex, visited, result_stack):
+    visited[current_vertex] = True
+
+    for neighbour_vertex in adjacency_list[current_vertex]:
+        if not visited[neighbour_vertex]:
+            bfs(adjacency_list, neighbour_vertex, visited, result_stack)
+    
+    result_stack.append(current_vertex)
+    
+# this algo works on directed acyclic graphs i.e DAGs
+def topo_sort(adjacency_list, no_of_vertices):
+    visited = [False] * no_of_vertices
+    result_stack = []
+
+    for i in range(no_of_vertices):
+        if not visited[i]:
+            bfs(adjacency_list, i, visited, result_stack)
+
+    result_stack.reverse()
+    
+    return result_stack
+
+
+adjacency_list = []
+no_of_vertices = int(input())
+for i in range(no_of_vertices):
+    adjacency_list.append([int(v) for v in input().split()])
+
+result = topo_sort(adjacency_list, no_of_vertices)
+print(result)
+
+"""
+6
+
+
+3
+1
+0 1
+0 2
+"""
