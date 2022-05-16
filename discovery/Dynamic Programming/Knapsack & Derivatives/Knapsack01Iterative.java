4d2c4a5a28bf5da7685f06a33fe4c4b1faa4bbfa
Akash Sharma <918527@cognizant.com>
2022-05-16 15:06:11 +0530

    Renaming

---
+++ discovery/Dynamic Programming/Knapsack & Derivatives/Knapsack01Iterative.java
@@ -0,0 +1,28 @@
+// BOTTOM UP APPROACH
+
+public class Knapsack01Iterative {
+    private static int knapsack(int[] itemProfits, int[] itemWeights, int knapsackCapacity) {
+        int noOfItems = itemProfits.length;
+
+        int[][] memoizationMatrix = new int[noOfItems+1][knapsackCapacity+1];
+        
+        for(int currentItemsIncluded = 1; currentItemsIncluded <= noOfItems; currentItemsIncluded++) {
+            for(int currentKnapsackCapacity = 1; currentKnapsackCapacity <= knapsackCapacity; currentKnapsackCapacity++) {
+                if(itemWeights[currentItemsIncluded - 1] > currentKnapsackCapacity)
+                    memoizationMatrix[currentItemsIncluded][currentKnapsackCapacity] = memoizationMatrix[currentItemsIncluded-1][currentKnapsackCapacity];
+                else
+                    memoizationMatrix[currentItemsIncluded][currentKnapsackCapacity] = Math.max(itemProfits[currentItemsIncluded - 1] + memoizationMatrix[currentItemsIncluded - 1][currentKnapsackCapacity - itemWeights[currentItemsIncluded - 1]], memoizationMatrix[currentItemsIncluded - 1][currentKnapsackCapacity]);
+            }
+        }
+
+        return memoizationMatrix[noOfItems][knapsackCapacity];
+    }
+
+    public static void main(String[] args) {
+        int[] itemWeights = new int[] { 7, 2, 3, 5, 6 };
+        int[] itemProfits = new int[] { 5, 4, 7, 1, 3 };
+        int knapsackCapacity = 10;
+
+        System.out.println(knapsack(itemProfits, itemWeights, knapsackCapacity));
+    }
+}
---
+++ discovery/Dynamic Programming/Knapsack & Derivatives/Knapsack01RecursiveMemoized.java
@@ -0,0 +1,30 @@
+// TOP DOWN APPROACH
+
+public class Knapsack01RecursiveMemoized {
+    private static int knapsack(int[] itemProfits, int[] itemWeights, int knapsackCapacity, int currentItemIndex, int[][] memoizationMatrix) {
+        if(currentItemIndex >= itemProfits.length || itemWeights[currentItemIndex] > knapsackCapacity)
+            return 0;
+
+        if(memoizationMatrix[currentItemIndex+1][knapsackCapacity] != 0)
+            return memoizationMatrix[currentItemIndex+1][knapsackCapacity];
+        
+        int resultWhenIncludingCurrentItem = itemProfits[currentItemIndex] + knapsack(itemProfits, itemWeights, knapsackCapacity-itemWeights[currentItemIndex], currentItemIndex+1, memoizationMatrix);
+        int resultWhenExcludingCurrentItem = knapsack(itemProfits, itemWeights, knapsackCapacity, currentItemIndex+1, memoizationMatrix);
+        int optimalResult = Math.max(resultWhenExcludingCurrentItem, resultWhenIncludingCurrentItem);
+
+        memoizationMatrix[currentItemIndex+1][knapsackCapacity] = optimalResult;
+
+        return optimalResult;
+    }
+
+    public static void main(String[] args) {
+        int[] itemWeights = new int[] { 7, 2, 3, 5, 6 };
+        int[] itemProfits = new int[] { 5, 4, 7, 1, 3 };
+        int knapsackCapacity = 10;
+        int noOfItems = itemProfits.length;
+
+        int[][] memoizationMatrix = new int[noOfItems+1][knapsackCapacity+1];
+
+        System.out.println(knapsack(itemProfits, itemWeights, knapsackCapacity, 0, memoizationMatrix));
+    }
+}
