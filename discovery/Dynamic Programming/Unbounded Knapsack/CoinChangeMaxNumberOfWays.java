d9fae1b3dde4fcd6b203df9e6d1c51920f4b3326
Akash Sharma <918527@cognizant.com>
2022-05-29 15:23:12 +0530

    coin change 2d

--- discovery/Dynamic Programming/Unbounded Knapsack/CoinChangeMaxNumberOfWays.java
+++ discovery/Dynamic Programming/Unbounded Knapsack/CoinChangeMaxNumberOfWays.java
@@ -26,6 +26,7 @@ public class CoinChangeMaxNumberOfWays {
         // this algo will give an anwer of 5 
         // this is because it creates all combinations
         // all the permutations are -> 11111, 1112, 113, 122, 23
+        // if we use 2D memoization table then automatically combinations will be created
 
         int noOfItems = coinProfits.length;
         int[] memoizationArray = new int[targetSum + 1];
@@ -43,11 +44,38 @@ public class CoinChangeMaxNumberOfWays {
         return memoizationArray[targetSum];
     }
 
+    public static int coinChangeAllCombinations2D(int[] coinProfits, int targetSum) {
+        // this algo uses 2D memoization array
+        // this algo will give an anwer of 5 
+        // this is because it creates all combinations
+        // all the permutations are -> 11111, 1112, 113, 122, 23
+        // if we observe closely then this is similar of above 1D approach but uses more space
+
+        int noOfItems = coinProfits.length;
+        int[][] tabulationMatrix = new int[noOfItems + 1][targetSum + 1];
+        
+        for(int i = 0; i <= noOfItems; i++) {
+            tabulationMatrix[i][0] = 1;
+        }
+
+        for(int currentItem = 1; currentItem <= noOfItems; currentItem++) {
+            for(int currentTargetSum = 1; currentTargetSum <= targetSum; currentTargetSum++) {
+                if(currentTargetSum >= coinProfits[currentItem - 1])
+                    tabulationMatrix[currentItem][currentTargetSum] += tabulationMatrix[currentItem-1][currentTargetSum] + tabulationMatrix[currentItem][currentTargetSum - coinProfits[currentItem - 1]];
+                else
+                    tabulationMatrix[currentItem][currentTargetSum] = tabulationMatrix[currentItem-1][currentTargetSum];
+            }
+        }
+
+        return tabulationMatrix[noOfItems][targetSum];
+    }
+
     public static void main(String[] args) {
         int[] coinProfits = new int[] { 1, 2, 3 };
         int targetSum = 5;
 
         System.out.println(coinChangeAllPermutations(coinProfits, targetSum));
         System.out.println(coinChangeAllCombinations(coinProfits, targetSum));
+        System.out.println(coinChangeAllCombinations2D(coinProfits, targetSum));
     }
 }
