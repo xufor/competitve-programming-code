3209674d53a00a0434439dfefbfbcb2cf01b1ef3
Akash Sharma <918527@cognizant.com>
2022-06-10 15:36:07 +0530

    two sum in python refactored

--- leetcode/leetcode_1.py
+++ leetcode/leetcode_1.py
@@ -1,9 +1,8 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for i, num in enumerate(nums):
-            complement_index = map.get(target - num)
-            if complement_index == None:
-                map[num] = i
-            else:
-                return sorted([i, complement_index])
+def twoSum(nums, target):
+    map = {}
+    for i, num in enumerate(nums):
+        complement_index = map.get(target - num)
+        if complement_index == None:
+            map[num] = i
+        else:
+            return sorted([i, complement_index])
