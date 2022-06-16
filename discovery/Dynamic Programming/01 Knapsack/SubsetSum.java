10b452b8163b319e4090c875308187954a6c4284
Akash Sharma <918527@cognizant.com>
2022-06-16 19:53:01 +0530

    one optimization

--- discovery/Dynamic Programming/01 Knapsack/SubsetSum.java
+++ discovery/Dynamic Programming/01 Knapsack/SubsetSum.java
@@ -7,8 +7,9 @@ public class SubsetSum {
         for (int k = 0; k <= noOfItems; k++)
         tabulationMatrix[k][0] = true;
         
-        for (int k = 1; k <= targetSum; k++)
-        tabulationMatrix[0][k] = false;
+        // this loop is not needed as default values of boolean is already false
+        // for (int k = 1; k <= targetSum; k++)
+        // tabulationMatrix[0][k] = false;
 
         // dynamic programming logic
         for(int currentItemsIncluded = 1; currentItemsIncluded <= noOfItems; currentItemsIncluded++) {
