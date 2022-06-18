44e9212b31442803e8a8f5a9f36f4855970414c8
Akash Sharma <lucky1612000@gmail.com>
2022-06-18 19:51:03 +0530

    removed a function to void confusion

--- discovery/Dynamic Programming/Unbounded Knapsack/UnboundedKnapsackRecursive.java
+++ discovery/Dynamic Programming/Unbounded Knapsack/UnboundedKnapsackRecursive.java
@@ -1,27 +1,12 @@
 public class UnboundedKnapsackRecursive {
-    public static int knapsackV1(int[] itemProfits, int[] itemWeights, int knapsackCapacity, int currentItemIndex) {
+    public static int knapsack(int[] itemProfits, int[] itemWeights, int knapsackCapacity, int currentItemIndex) {
         int noOfItems = itemProfits.length;
         if(knapsackCapacity < itemWeights[currentItemIndex])
             return 0;
         else {
             int currentMaxProfit = 0;
             for(int i = 0; i < noOfItems; i++)
-                currentMaxProfit = itemProfits[i] + knapsackV1(itemProfits, itemWeights, knapsackCapacity - itemWeights[i], i);
-            return currentMaxProfit;
-        }
-    }
-
-    
-    public static int knapsackV2(int[] itemProfits, int[] itemWeights, int knapsackCapacity, int maxProfit) {
-        int noOfItems = itemProfits.length;
-        if(knapsackCapacity == 0)
-            return maxProfit;
-        else if(knapsackCapacity < 0)
-            return -1;
-        else {
-            int currentMaxProfit = -1;
-            for(int i = 0; i < noOfItems; i++)
-                currentMaxProfit = Math.max(knapsackV2(itemProfits, itemWeights, knapsackCapacity - itemWeights[i], maxProfit + itemProfits[i]), currentMaxProfit);
+                currentMaxProfit = itemProfits[i] + knapsack(itemProfits, itemWeights, knapsackCapacity - itemWeights[i], i);
             return currentMaxProfit;
         }
     }
@@ -31,7 +16,6 @@ public class UnboundedKnapsackRecursive {
         int[] itemProfits = new int[] { 1, 2, 3 };
         int knapsackCapacity = 5;
 
-        System.out.println(knapsackV1(itemProfits, itemWeights, knapsackCapacity, 0));
-        System.out.println(knapsackV2(itemProfits, itemWeights, knapsackCapacity, 0));
+        System.out.println(knapsack(itemProfits, itemWeights, knapsackCapacity, 0));
     }
 } 
